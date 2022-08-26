8b9162d641910686b2761cf0f1977db275e392fe
Zachary Soliman <zachsoliman1@gmail.com>
2022-08-26 12:48:38 -0400

    updates

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -21,8 +21,11 @@ class ReviewsController < ApplicationController
 
   def post_review
     user = User.find_by(user_name: params[:user_name])
+
     movie = Movie.find_by(title: params[:movie_name])
+
     review = Review.create(line_1: params[:line_1], line_2: params[:line_2], line_3: params[:line_3], star_num: params[:star_num], user_id: user["id"], movie_id: movie["id"])
+    
     render json: review
   end
 end
